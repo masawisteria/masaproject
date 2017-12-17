b2de24c07b97adba9694a35a860390dc5896c6ca
masawisteria <masataka.the.wisteria@gmail.com>
2017-12-17 17:04:01 +0900

    teamproject

---
+++ Statisticsproject.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+import sklearn as skl 
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+
+#df = pd.read_csv("F:/Statistic/TeamProjectdocdata17/traintest2017.csv")
+#print (df)
